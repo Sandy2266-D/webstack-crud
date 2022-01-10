030c39ca337dc6735772559ec7af1099f6a72f6c
sandy2266-D <santhoshdurai2266@gmail.com>
2022-01-10 09:32:03 +0530

    make it better

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
 // const usersUrl = 'http://localhost:3003/users';
-const usersUrl = 'https://localhost:5000';
+
 
 export const getUsers = async (id) => {
     id = id || '';
-    return await axios.get(`${usersUrl}/users/${id}`);
+    return await axios.get(`/${id}`);
 }
 
 export const addUser = async (user) => {
-    return await axios.post(`${usersUrl}/users/add`, user);
+    return await axios.post(`/add`, user);
 }
 
 export const deleteUser = async (id) => {
-    return await axios.delete(`${usersUrl}/users/${id}`);
+    return await axios.delete(`/${id}`);
 }
 
 export const editUser = async (id, user) => {
-    return await axios.put(`${usersUrl}/users/${id}`, user)
+    return await axios.put(`/${id}`, user)
 }
