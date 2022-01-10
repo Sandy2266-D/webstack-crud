b6d94c2072ab54b8cca80fbfc6d3a002fb79bdd6
sandy2266-D <santhoshdurai2266@gmail.com>
2022-01-10 09:23:28 +0530

    make it better

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const usersUrl = 'http://localhost:3003/users';
-const usersUrl = 'https://localhost:5000//users';
+const usersUrl = 'https://localhost:5000/users';
 
 export const getUsers = async (id) => {
     id = id || '';
